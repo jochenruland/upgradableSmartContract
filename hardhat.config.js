66a5c18c705f700c78a24c01b21d22930464e369
jochenruland <jochenhgruland@gmail.com>
2022-10-29 08:14:02 +0200

    ADD boilerplate configuration for mainnetFork

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,15 @@ require("@nomicfoundation/hardhat-toolbox");
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+// const ALCHEMY_API_KEY = "KEY";
 
+// Replace this private key with your Goerli account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+// const GOERLI_PRIVATE_KEY = "YOUR GOERLI PRIVATE KEY";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -23,36 +31,17 @@ module.exports = {
     //  network_id: "*",       // Any network (default: none)
     // },
     mainnetFork: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 7545,            // Standard Ethereum port (default: none)
+      url: "http://127.0.0.1:7545",
+      // accounts: [GOERLI_PRIVATE_KEY],
+      // host: "127.0.0.1",     // Localhost (default: none)
+      // port: 7545,            // Standard Ethereum port (default: none)
       network_id: "999",       // Any network (default: none)
       websocket: true
     },
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websocket: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
+    //goerli: {
+    //  url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+    //  accounts: [GOERLI_PRIVATE_KEY]
+    //}
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -60,7 +49,7 @@ module.exports = {
     timeout: 100000
   },
 
-  compilers: {
-    solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17"
   }
 }
