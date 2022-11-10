8c3c6cf233c971e2679209a0cef31ab527e113b3
jochenruland <jochenhgruland@gmail.com>
2022-11-10 08:50:09 +0100

    DEL unnecessary comments

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
@@ -19,17 +17,6 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
 
   networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
     mainnetFork: {
       url: "http://127.0.0.1:7545",
       // accounts: [GOERLI_PRIVATE_KEY],
